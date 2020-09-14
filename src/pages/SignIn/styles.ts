7e9c7b98dbde0abe17f39cc8fbf8456c558b107f
Matheus Heck <matheus@conduite.tv>
2020-09-14 14:11:58 +0200

    add font family

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,5 +1,17 @@
 import styled from 'styled-components/native';
 
-const Container = styled.View``;
+const Container = styled.View`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
-export { Container };
+const Title = styled.Text`
+  margin: 64px 0 24px;
+  font-size: 20px;
+  color: #f4ede8;
+  font-family: 'RobotoSlab-Medium';
+`;
+
+export { Container, Title };
