bee7605399ef5a4ddba5d5ee7a63d06ce14d5bb0
Matheus Heck <matheus@conduite.tv>
2020-09-14 16:09:21 +0200

    style signin page

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components/native';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 
-const Container = styled.View`
+export const Container = styled.View`
   flex: 1;
   display: flex;
   align-items: center;
@@ -9,11 +10,41 @@ const Container = styled.View`
   padding: 0 30px;
 `;
 
-const Title = styled.Text`
+export const Title = styled.Text`
   margin: 64px 0 24px;
   font-size: 20px;
   color: #f4ede8;
   font-family: 'RobotoSlab-Medium';
 `;
 
-export { Container, Title };
+export const ForgotPassword = styled.TouchableOpacity`
+  margin-top: 24px;
+`;
+
+export const ForgotPasswordText = styled.Text`
+  color: #fff;
+  font-size: 16px;
+  font-family: 'RobotoSlab-Regular';
+`;
+
+export const CreateAccount = styled.TouchableOpacity`
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background: #312e38;
+  border-top-width: 1px;
+  border-top-color: #232129;
+  padding: 16px 0 ${getBottomSpace()}px;
+
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+`;
+
+export const CreateAccountText = styled.Text`
+  color: #ff9000;
+  font-size: 16px;
+  font-family: 'RobotoSlab-Regular';
+  margin-left: 16px;
+`;
